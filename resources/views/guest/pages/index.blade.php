73c194ae12370a0337ce1429db96c22b1ab9ae82
GencoMattia <gencomattia@hotmail.it>
2024-07-18 15:37:01 +0200

    Created TrainController and Train model

---
+++ resources/views/guest/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends("layout.app")
+
+@section("page-title", "Train Index")
+
+@section("main-content")
+    <main>
+        <h1>
+            Sono un Main-Content
+        </h1>
+    </main>
+@endsection
