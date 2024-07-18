e55e5b6e1282b4abc9f56bf8e5325ecd6720e651
GencoMattia <gencomattia@hotmail.it>
2024-07-18 15:52:40 +0200

    insert table paramers in train migration file

--- database/migrations/2024_07_18_133820_create_trains_table.php
+++ database/migrations/2024_07_18_133820_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("company", 50);
+            $table->string("departure_station", 50);
+            $table->string("arrival_station", 50);
+            $table->dateTime("departure_time");
+            $table->dateTime("arrival_time");
+            $table->string("train_code", 10)->unique();
+            $table->unsignedInteger("carriage_count");
+            $table->boolean("on_time")->default(true);
+            $table->boolean("cancelled")->default(false);
             $table->timestamps();
         });
     }
